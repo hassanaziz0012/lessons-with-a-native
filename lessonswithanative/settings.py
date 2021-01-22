b3145a53ce6a14a75538d89f992e352a4cc55074
Hassan Aziz <hassanaziz0012@gmail.com>
2021-01-22 15:29:53 +0500

    Settings

--- lessonswithanative/settings.py
+++ lessonswithanative/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p((n(e#wgv@ivv(cjm*s)vpm23%!vu(0ezwrzz_tzz%7%je2p4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lessons-with-a-native.herokuapp.com']
+ALLOWED_HOSTS = ['josiahtrotter.herokuapp.com']
 
 
 # Application definition
